698403551f0152c561c85388087f85bc173390a8
Loading <376569230@qq.com>
2016-03-19 14:37:43 +0800

    Initial Commit

---
+++ LoadWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadWeibo
+//
+//  Created by Loading张 on 16/3/19.
+//  Copyright © 2016年 load. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
